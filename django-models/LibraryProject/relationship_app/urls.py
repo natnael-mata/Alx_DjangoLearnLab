da55ea32ed90332c55f42bc4d6eba2f5fc288370
natnael-mata <nati.sami.mata@gmail.com>
2025-11-23 02:04:55 +0300

    view url fixed

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
 from . import views
 from .views import list_books, LibraryDetailView
+from django.contrib.auth.views import LoginView, LogoutView
+
+app_name = 'relationship_app'
+
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+    path('register/', views.register.as_view(), name='register'),
 ]
 
+    
+   
