43f2ea29b780d8e3b7a1107a430fad9f327080c2
natnael-mata <nati.sami.mata@gmail.com>
2025-11-23 20:41:51 +0300

    second push

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,18 +1,23 @@
 from django.urls import path
 from . import views
-from .views import list_books, LibraryDetailView
-from django.contrib.auth.views import LoginView, LogoutView
+from .views.admin_view import admin_dashboard
+from .views.librarian_view import librarian_dashboard
+from .views.member_view import member_dashboard
 
 app_name = 'relationship_app'
 
-
 urlpatterns = [
+    # Books
     path('books/', views.list_books, name='list_books'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+
+    # Authentication
     path('login/', views.user_login, name='login'),
     path('register/', views.register, name='register'),
     path('logout/', views.user_logout, name='logout'),
-]
 
-    
-   
+    # Role-based dashboards
+    path('admin-dashboard/', admin_dashboard, name='admin_dashboard'),
+    path('librarian-dashboard/', librarian_dashboard, name='librarian_dashboard'),
+    path('member-dashboard/', member_dashboard, name='member_dashboard'),
+]
