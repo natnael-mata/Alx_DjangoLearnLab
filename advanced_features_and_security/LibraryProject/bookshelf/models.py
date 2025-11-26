c2709cd5c149052a012b3c297b888864ffa20540
natnael-mata <nati.sami.mata@gmail.com>
2025-11-26 23:40:51 +0300

    base user

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,6 +1,41 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 # Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, date_of_birth=None, profile_photo=None, **extra_fields):
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        email = self.normalize_email(email)
+
+        user = self.model(
+            email=email,
+            date_of_birth=date_of_birth,
+            profile_photo=profile_photo,
+            **extra_fields
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, date_of_birth=None, profile_photo=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if password is None:
+            raise ValueError("Superusers must have a password")
+
+        return self.create_user(
+            email=email,
+            password=password,
+            date_of_birth=date_of_birth,
+            profile_photo=profile_photo,
+            **extra_fields
+        )
+
+
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
@@ -13,3 +48,4 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+    
