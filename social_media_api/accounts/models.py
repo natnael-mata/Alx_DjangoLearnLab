9a794c2455adefb375ddc736fdff7f4986303e4b
natnael-mata <nati.sami.mata@gmail.com>
2025-12-14 21:02:21 +0300

    second push

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    bio = models.TextField(blank=True, null=True)
+
+    profile_picture = models.ImageField(
+        upload_to='profile_pictures/',
+        blank=True,
+        null=True
+    )
+
+    followers = models.ManyToManyField(
+        'self',
+        symmetrical=False,
+        related_name='following',
+        blank=True
+    )
